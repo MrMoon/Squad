dcb2b23b5c113bb455a4d8524465320fd0c43057
MohammadRimawi <59421484+MohammadRimawi@users.noreply.github.com>
2020-09-14 23:38:47 +0300

    Dark Mode is added

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { FeedComponent } from './feed/feed.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    FeedComponent
   ],
   imports: [
     BrowserModule,
