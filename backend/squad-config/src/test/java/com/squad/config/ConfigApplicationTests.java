79a28a5acda52d4e02c19985168885f390f6d0c4
MrMoon <abuamaramstudy@gmail.com>
2020-09-14 09:05:37 +0300

    Initial commit

---
+++ backend/squad-config/src/test/java/com/squad/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
