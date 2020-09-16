325b569496925648328432db6b79961214554852
MrMoon <abuamaramstudy@gmail.com>
2020-09-16 15:54:38 +0300

    commit

---
+++ backend/squad-group/src/main/java/com/squad/group/GroupApplication.java
@@ -0,0 +1,13 @@
+package com.squad.group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupApplication.class , args);
+    }
+
+}
