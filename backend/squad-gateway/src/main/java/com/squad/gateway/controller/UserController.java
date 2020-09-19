b7f1c8e1bfb7bf0b9ef970a95a4afc244832cab6
MrMoon <abuamaramstudy@gmail.com>
2020-09-19 22:17:27 +0300

    commit user api routing correction

--- backend/squad-gateway/src/main/java/com/squad/gateway/controller/UserController.java
+++ backend/squad-gateway/src/main/java/com/squad/gateway/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.text.ParseException;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/current")
 @RequiredArgsConstructor
 public class UserController {
 
